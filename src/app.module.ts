907546028960c28960070bb85481391e17fe38da
SKupisz <szym-ku@wp.pl>
2022-08-24 19:30:53 +0200

    Adjusting application to heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HandsModule } from './hands/hands.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HandsModule],
+  imports: [HandsModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ".env"
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
